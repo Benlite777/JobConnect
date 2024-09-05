dbe02c473527a796bc9f3d8716a970d3c9a354f3
Benhur <imelite512@gmail.com>
2024-09-05 19:47:37 +0530

    First Commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,32 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+# Exit script on error
+set -e
+
+# Define virtual environment and requirements file
+VENV_DIR="venv"
+REQUIREMENTS_FILE="requirements.txt"
+
+# Create a virtual environment
+python -m venv $VENV_DIR
+
+# Activate the virtual environment
+source $VENV_DIR/bin/activate  # For Windows: $VENV_DIR\Scripts\activate
+
+# Upgrade pip to the latest version
+pip install --upgrade pip
+
+# Install the dependencies
+pip install -r $REQUIREMENTS_FILE
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run database migrations
+python manage.py migrate
+
+# Additional build steps (if any)
+# ...
+
+# Deactivate the virtual environment
+deactivate
