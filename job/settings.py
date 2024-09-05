546a7b74f9505a6ecab79e5d586bc655c049de2a
Benhur <imelite512@gmail.com>
2024-09-05 18:21:49 +0530

    First Commit

--- job/settings.py
+++ job/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1:8000, djobportal.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- job/wsgi.py
+++ job/wsgi.py
@@ -16,3 +16,5 @@ from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 application = WhiteNoise(application)
 
+
+app = application
